ac1a42b5d74b4f41618a821a5030723a429320d0
Aulipe <johnbostl@hotmail.com>
2024-12-08 01:48:13 +0100

    Strings

---
+++ Strings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Code read from top to buttom
+
+    string message1 = "hello world app\n";
+    string message2 = "coded by jan bostl\n";
+
+    // String combined
+    string message3 = message1 + message2;
+
+    cout << message3;
+
+    cin.get();
+
+    return 0;
+}
+
+
